0ccd724be655a1a844b46b54aa2bbc9454e9eeee
luismedinacoca <luismedinacoca@gmail.com>
2020-11-09 23:46:32 -0300

    JavaScript 26 Document object properties and methods - part 1

---
+++ javaScript_26/app.js
@@ -0,0 +1,57 @@
+let doc
+doc = document
+console.log(doc)
+
+console.log(" ")
+
+// document.all[0] = <html></html>
+doc = document.all[0]
+console.log(doc)
+
+console.log(" ")
+
+// document.all[1] = <head></head>
+doc = document.all[1]
+console.log(doc)
+
+console.log(" ")
+
+// document.head
+doc = document.head
+console.log(doc)
+
+console.log(" ")
+
+// document.body
+doc = document.body
+console.log(doc)
+
+console.log(" ")
+
+// document.doctype
+doc = document.doctype
+console.log(doc)
+
+console.log(" ")
+
+// document.domain = localhost
+doc = document.domain
+console.log(doc)
+
+console.log(" ")
+
+// document.URL
+doc = document.URL
+console.log(doc)
+
+console.log(" ")
+
+// document.characterSet
+doc = document.characterSet
+console.log(doc)
+
+console.log(" ")
+
+// document.contentType
+doc = document.contentType
+console.log(doc)
